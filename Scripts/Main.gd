8d14045da4d4a7b701f2f9857787bbae550093c2
Alexandre Massaro <alexandremassaro@gmail.com>
2021-10-11 14:48:04 -0300

    Reworked Character movement due to vertical jittering and eventually clipping through the floor.
    Implemented game over mechanic and restart the map
    
    Created Pyramid spike obstacle

---
+++ Scripts/Main.gd
@@ -0,0 +1,25 @@
+extends Spatial
+
+export (PackedScene) var player_scene = preload("res://Scenes/MainScenes/Player.tscn")
+
+onready var player
+onready var game_over_menu = get_node("GameOver")
+
+func _ready():
+	player = player_scene.instance()
+	
+	add_child(player)
+	player.connect("player_died", self, "player_died")
+	
+	game_over_menu.get_node("CenterContainer/VBoxContainer/HBoxContainer/Restart").connect("pressed", self, "restart")
+
+
+func restart():
+# warning-ignore:return_value_discarded
+	player.queue_free()
+	get_tree().reload_current_scene()
+	get_tree().paused = false
+
+func player_died():
+	game_over_menu.visible = true
+	get_tree().paused = true
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,54 +1,117 @@
-extends Spatial
+extends KinematicBody
+class_name Player
 
-onready var body = get_node("RigidBody")
+signal player_died
+
 onready var camera_gimble = get_node("CameraGimble")
-onready var floor_collider = get_node("FloorCollider")
+onready var camera : Camera = get_node("CameraGimble/Camera")
 
-var floor_contacts := 0
+const ORTH_CAMERA_SIZE = 15.0
+const ORTH_H_OFFSET = 3.0
+const ORTH_V_OFFSET = 2.0
+const ORTH_CAMERA_ROTATION = 0.0
 
-var new_max_vel = 0.0
-var new_min_vel = 10000.0
-var median_vel = []
+const PERSP_CAMERA_DISTANCE = 5.0
+const PERSP_H_OFFSET = 0.0
+const PERSP_V_OFFSET = 1.0
+const PERSP_CAMERA_ROTATION = -90.0
 
-var force = 0.0
-var on_the_ground = false
+const CAMERA_INTERPOLATION_SPEED = 0.5
 
-func _process(_delta):
+const FORWARD_DIRECTION := Vector3.RIGHT
+const JUMP_LINEAR_VELOCITY = 13.8
+const JUMP_ANGULAR_VELOCITY = -2.0
+const GRAVITY_MULTIPLIER = 5.0
+
+const MAX_SPEED = 10.0
+
+var gravity_magnitude : float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var velocity := Vector3.ZERO
+var snap_vector := Vector3.ZERO
+
+
+func _ready():
+	camera.size = ORTH_CAMERA_SIZE
+	camera.h_offset = ORTH_H_OFFSET
+	camera.v_offset = ORTH_V_OFFSET
+
+
+func _input(event):
+	if event.is_action_pressed("change_perspective"):
+		change_perpective()
+
+
+func _physics_process(delta):
+	apply_movement()
+	apply_gravity(delta)
+	update_snap_vector()
+	jump()
+	velocity = move_and_slide_with_snap(velocity, snap_vector, Vector3.UP)
+
+
+func apply_movement():
+	velocity.x = MAX_SPEED
+
+
+func apply_gravity(delta):
+	velocity.y += -gravity_magnitude * GRAVITY_MULTIPLIER * delta
+	velocity.y = clamp(velocity.y, -gravity_magnitude * GRAVITY_MULTIPLIER, JUMP_LINEAR_VELOCITY)
+
+
+func update_snap_vector():
+#	snap_vector = -get_floor_normal() if is_on_floor() else Vector3.DOWN
+	snap_vector = Vector3.DOWN
 	
-	camera_gimble.translation = body.translation
-	
-	if Input.is_action_pressed("jump"):
-		jump()
-		
-func _physics_process(_delta):
-	if body.linear_velocity.x <= 10:
-		force += 0.2
-	else:
-		force = 0.0
-		
-	body.add_central_force(Vector3(force, 0, 0))
-	
-	on_the_ground = floor_contacts
-#	if not on_the_ground : print(on_the_ground)
-
 
 func jump():
-	if not on_the_ground:
-		return
+	if Input.is_action_pressed("jump") and is_on_floor():
+		snap_vector = Vector3.ZERO
+		velocity.y = JUMP_LINEAR_VELOCITY
+
+
+func change_perpective():
+	
+	var gimble_tween : Tween = get_node("CameraGimble/GimbleTween")
+	var camera_tween : Tween = get_node("CameraGimble/Camera/CameraTween")
+	
+	if camera.projection == Camera.PROJECTION_ORTHOGONAL:
+		camera.set_projection(Camera.PROJECTION_PERSPECTIVE)
+# warning-ignore:return_value_discarded
+		gimble_tween.interpolate_property(camera_gimble, "rotation_degrees:y", ORTH_CAMERA_ROTATION, PERSP_CAMERA_ROTATION, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+# warning-ignore:return_value_discarded
+		camera_tween.interpolate_property(camera, "h_offset", ORTH_H_OFFSET, PERSP_H_OFFSET, CAMERA_INTERPOLATION_SPEED, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+# warning-ignore:return_value_discarded
+		camera_tween.interpolate_property(camera, "v_offset", ORTH_V_OFFSET, PERSP_V_OFFSET, CAMERA_INTERPOLATION_SPEED, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+# warning-ignore:return_value_discarded
+		camera_tween.interpolate_property(camera, "translation:z", ORTH_CAMERA_SIZE, PERSP_CAMERA_DISTANCE, CAMERA_INTERPOLATION_SPEED, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+# warning-ignore:return_value_discarded
+		camera_tween.interpolate_property(camera, "size", ORTH_CAMERA_SIZE, PERSP_CAMERA_DISTANCE, CAMERA_INTERPOLATION_SPEED, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	else:
-		body.linear_velocity.y = 20.0
-		body.angular_velocity.z = -4.0
+		camera.set_projection(Camera.PROJECTION_ORTHOGONAL)
+# warning-ignore:return_value_discarded
+		gimble_tween.interpolate_property(camera_gimble, "rotation_degrees:y", PERSP_CAMERA_ROTATION, ORTH_CAMERA_ROTATION, CAMERA_INTERPOLATION_SPEED, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+# warning-ignore:return_value_discarded
+		camera_tween.interpolate_property(camera, "h_offset", PERSP_H_OFFSET, ORTH_H_OFFSET, CAMERA_INTERPOLATION_SPEED, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+# warning-ignore:return_value_discarded
+		camera_tween.interpolate_property(camera, "v_offset", PERSP_V_OFFSET, ORTH_V_OFFSET, CAMERA_INTERPOLATION_SPEED, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+# warning-ignore:return_value_discarded
+		camera_tween.interpolate_property(camera, "translation:z", PERSP_CAMERA_DISTANCE, ORTH_CAMERA_SIZE, CAMERA_INTERPOLATION_SPEED, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+# warning-ignore:return_value_discarded
+		camera_tween.interpolate_property(camera, "size", PERSP_CAMERA_DISTANCE, ORTH_CAMERA_SIZE, CAMERA_INTERPOLATION_SPEED, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	
+# warning-ignore:return_value_discarded
+	gimble_tween.start()
+# warning-ignore:return_value_discarded
+	camera_tween.start()
 
 
-func _on_FloorCollider_body_entered(body):
-	print("Touched Something")
-	if body.is_in_group("floor"):
-		print("Touched floor")
-		floor_contacts += 1
+func die():
+	# TODO: Show game over screen
+	# TODO: Pause game
+	emit_signal("player_died")
 
 
-func _on_FloorCollider_body_exited(body):
-	print("Left Something")
-	if body.is_in_group("floor"):
-		print("Left floor")
-		floor_contacts -= 1
+func _on_ObstacleCollider_body_entered(body):
+	if body.is_in_group("obstacle"):
+		die()
